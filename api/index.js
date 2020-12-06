32b03896bf52dffca77543c168150032dc89f42c
Matias <matias.kovero@gmail.com>
2020-12-06 13:38:39 +0200

    Edits to support vercel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,6 @@
-let express = require('express'),
-    request = require('request'),
-    cors = require('cors');
-
-let app = express();
+const app = require('express')();
+const request = require('request');
+const cors = require('cors');
 
 const root = 'https://fantasy.premierleague.com';
 
@@ -12,9 +10,12 @@ app.options('*', cors()) // Enable pre-flight across-the-board
 
 /**
  * Handle request to the API endpoint
+ * Caching from: https://vercel.com/docs/edge-network/caching 
  */
 app.use('/api', (req, res) => {
-  req.pipe(request(`${root}/api${req.url}`)).pipe(res);
+  req.pipe(request(`${root}/api${req.url}`)).on('response', (res) => {
+    res.headers['cache-control'] = 's-max-age=1, stale-while-revalidate';
+  }).pipe(res);
 });
 
 /**
@@ -29,4 +30,4 @@ app.use('/dist', (req, res) => {
  */
 app.use('/static', express.static('public'));
 
-app.listen();
+module.exports = app;
